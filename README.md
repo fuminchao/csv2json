b6b6abd182e4925e288760ca4f42afb60724d09d
Minchao Fu <mincfu@ebay.com>
2019-05-13 13:52:23 +0800

    initial version

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # csv2json
 Parse CSV into JSON Object
+
+# Sample
+
+Code
+```
+let json = await readCSVasJson(fs.createReadStream(file, {encoding: 'utf-8'}));
+```
+
+Supported CSV
+```
+# Comment supported
+col1,col2,col3
+text1,text2,text3
+text1,text2,"text3 ,test3 """
+```
+
+Output JSON
+```
+[
+  {col1: "text1", col2: "text2", col3: "text3"},
+  {col1: "text1", col2: "text2", col3: "text3 ,test3 \""}
+]
+```
---
+++ index.js
@@ -0,0 +1,81 @@
+'use strict';
+
+const readline = require('readline');
+
+function tokenize(line) {
+
+  const tokens = [];
+  const currentToken = [];
+
+  tokens.push = function(str) {
+    return Array.prototype.push.call(this, str.replace(/^\s*"(.*)"\s*$/, '$1').replace(/""/g, "\""));
+  };
+
+  line.split(/,/).forEach((t) => {
+
+    if (currentToken.length > 0) {
+      if (/"\s*$/.test(t)) {
+        currentToken.push(t);
+        tokens.push(currentToken.join(',').trim());
+        currentToken.length = 0;
+      } else {
+        currentToken.push(t);
+      }
+      return;
+    }
+
+    if (/^\s*"/.test(t)) {
+      currentToken.push(t);
+      return;
+    }
+
+    tokens.push(t);
+  });
+
+  return tokens;
+}
+
+const readCSVasJson = (inputStream) => new Promise((resolve, reject) => {
+
+  const rl = readline.createInterface({
+    input: inputStream,
+    crlfDelay: Infinity,
+  });
+
+  const results = [];
+
+  let readLine;
+  const parsers = [];
+  const readSchema = (line) => {
+
+    tokenize(line).map((k, i) => parsers.push((json, tokens) => json[k] = tokens[i]));
+
+    readLine = (data) => {
+      let res = {};
+      tokenize(data).map((k, i, arr) => {
+        (parsers[i] || (() => {}))(res, arr);
+      });
+
+      results.push(res);
+    };
+  };
+
+  readLine = readSchema;
+
+  rl.on('line', (line) => {
+
+    if (line.trim() === '' || /^\s*#/.test(line)) {
+      return;
+    }
+    readLine(line);
+  });
+
+  rl.on('close', () => resolve(results));
+});
+
+module.exports = {
+  /**
+   * Read CSV file as JSON, whose keys come from the first line of the CSV file
+   */
+  readCSVasJson,
+};
